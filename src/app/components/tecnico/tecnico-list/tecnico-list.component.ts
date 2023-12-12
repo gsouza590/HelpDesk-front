dce11a6c37328632718b7f2a63d93144d3a8af53
gsouza590 <gsouza590@gmail.com>
2023-12-12 17:34:16 -0300

    Create TecnicoListComponent and Model Tecnico

---
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTableDataSource } from "@angular/material/table";
+import { Tecnico } from "src/app/models/tecnico";
+
+@Component({
+  selector: "app-tecnico-list",
+  templateUrl: "./tecnico-list.component.html",
+  styleUrls: ["./tecnico-list.component.css"],
+})
+export class TecnicoListComponent implements OnInit {
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: "Gabriel",
+      cpf: "123.456.789-10",
+      email: "gsouza590@gmail.com",
+      senha: "1234",
+      perfis: ["0"],
+      dataCriacao: [12 / 12 / 2023],
+    },
+  ];
+  displayedColumns: string[] = [
+    "position",
+    "name",
+    "weight",
+    "symbol",
+    "acoes",
+  ];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+}
