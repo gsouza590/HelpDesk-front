a1c61e18cb93bc68c709665412cf9c4ced75d586
gsouza590 <gsouza590@gmail.com>
2024-01-03 15:52:01 -0300

    Implement methods Tecnico-Create

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -4,9 +4,13 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatButtonModule } from "@angular/material/button";
-import { FormControl, Validators } from "@angular/forms";
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { NgxMaskDirective } from "ngx-mask";
+import { TecnicoService } from "src/app/services/tecnico.service";
+import { Tecnico } from "src/app/models/tecnico";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { Router, RouterModule } from "@angular/router";
 @Component({
   selector: "app-tecnico-create",
   standalone: true,
@@ -17,18 +21,34 @@ import { NgxMaskDirective } from "ngx-mask";
     MatIconModule,
     MatButtonModule,
     ReactiveFormsModule,
-    NgxMaskDirective
+    FormsModule, // Adicionado FormsModule
+    ToastrModule,
+    NgxMaskDirective,
+    RouterModule,
   ],
   templateUrl: "./tecnico-create.component.html",
   styleUrl: "./tecnico-create.component.css",
 })
 export class TecnicoCreateComponent implements OnInit {
+  tecnico: Tecnico = {
+    id: "",
+    nome: "",
+    cpf: "",
+    email: "",
+    senha: "",
+    perfis: [],
+    dataCriacao: "",
+  };
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, Validators.required);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() {}
+  constructor(
+    private service: TecnicoService,
+    private toast: ToastrService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
@@ -36,5 +56,32 @@ export class TecnicoCreateComponent implements OnInit {
     return (
       this.nome.valid && this.cpf.valid && this.email.valid && this.senha.valid
     );
+  }
+
+  create(): void {
+    this.service.create(this.tecnico).subscribe(
+      () => {
+        this.toast.success("Técnico cadastrado com sucesso", "Cadastro");
+        this.router.navigate(["tecnicos"]);
+      },
+      (ex) => {
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element: { message: string | undefined }) => {
+            this.toast.error(element.message || 'Erro desconhecido');
+          });
+          
+        } else {
+          this.toast.error(ex.error.message);
+        }
+      }
+    );
+  }
+
+  addPerfil(perfil: any): void {
+    if (this.tecnico.perfis.includes(perfil)) {
+      this.tecnico.perfis.splice(this.tecnico.perfis.indexOf(perfil), 1);
+    } else {
+      this.tecnico.perfis.push(perfil);
+    }
   }
 }
